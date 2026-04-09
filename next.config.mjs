6871bff30187deaf41b884e3b85e81e40aa49c62
StarkBBK <starkbbk@StarkBBKs-MacBook-Air.local>
2026-04-09 13:04:51 +0530

    fix(frontend): add Referrer-Policy for better google auth compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -53,6 +53,10 @@ const nextConfig = {
             key: 'Cross-Origin-Opener-Policy',
             value: 'same-origin-allow-popups',
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
         ],
       },
     ];
